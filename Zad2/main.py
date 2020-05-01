062538fa7b3ec5d124203270dd8cb0b97ea7c786
Jakub Bogdan <kubab1998@o2.pl>
2020-05-01 16:42:48 +0200

    Znowaczony pyton

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,38 +1,77 @@
 import FileOperations
+
 import pandas as pd
-import neurolab as nl
+import numpy as np
+import pathlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
 
 
 def calculate_outputs(dataframe, threshold):
     dataframe['output'] = [1 if x < threshold else 0 for x in dataframe['distance']]
 
 
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64, activation='relu', input_shape=[len(train_dataset[0].keys())]),
+        layers.Dense(64, activation='relu',),
+        layers.Dense(2)
+    ])
+
+    model.compile(loss='mse', optimizer = tf.keras.optimizers.RMSprop(0.001), metrics=['mae', 'mse'])
+    return model
+
+
 if __name__ == '__main__':
     runs = []
-    train_values = []
+    train_values_measurements = []
+    train_values_references = []
     outputs = []
     solution = pd.DataFrame
+    solution_measurements = pd.DataFrame
+    solution_references = pd.DataFrame
+    train_dataset = []
+    test_dataset = []
+    train_dataset_labels = []
+    test_dataset_labels = []
+
     for i in range(12):
         runs.append(FileOperations.FileOperations.load_data_from_excel
                     (r'pozyxAPI_dane_pomiarowe\pozyxAPI_only_localization_measurement' + str(i+1) + '.xlsx', "measurement", "D:H"))
-        x1 = runs[i]['measurement x']
-        x2 = runs[i]['reference x']
-        y1 = runs[i]['measurement y']
-        y2 = runs[i]['reference y']
-        runs[i]['distance'] = pow(((x1 - x2)**2 + (y1 - y2)**2), 0.5)
-        calculate_outputs(runs[i], 200)
-        train_values.append(runs[i][['measurement x', 'measurement y']])
-        outputs.append(runs[i][['output']])
+        # train_values_measurements.append(runs[i][['measurement x', 'measurement y']])
+        # train_values_references.append(runs[i][['reference x', 'reference y']])
+        train_dataset.append(runs[i].sample(frac=0.8, random_state=0))
+        test_dataset.append(runs[i].drop(train_dataset[i].index))
+        train_dataset_labels.append(train_dataset[i][['reference x', 'reference y']])
+        test_dataset_labels.append(test_dataset[i][['reference x', 'reference y']])
+        train_dataset[i] = train_dataset[i][['measurement x', 'measurement y']]
+        test_dataset[i] = test_dataset[i][['measurement x', 'measurement y']]
+
     solution = FileOperations.FileOperations.load_data_from_excel(r'pozyxAPI_only_localization_dane_testowe_i_dystrybuanta.xlsx', "pomiar", "D:M")
-    print(runs)
+    solution_measurements = solution[['measurement x', 'measurement y']].dropna()
+    solution_references = solution[['reference x', 'reference y']].dropna()
 
-    # liczba par punktów - liczba wejść
-    # pierwszy argument pary - minimalna wartosc liczby wejsciowej
-    # drugi argument pary - maksymalna wartosc liczby wejsciowej
-    # 1 - liczba neuronów
-    net = nl.net.newp([[-500, 7000], [-1000, 5000]], 1)
+    model = build_model()
+    history = model.fit(train_dataset[0], train_dataset_labels[0], epochs=20, validation_split=0.2, verbose=0, callbacks=[tfdocs.modeling.EpochDots()])
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    hist.tail()
 
-    # epochs - liczba cykli
-    # show - co ile cykli wypisać na terminal
-    # lr - learning rate, step size that`s taken to adjust the parameters so that we move towards the end goal
-    error = net.train(train_values[0].values.tolist(), outputs[0].values.tolist(), epochs=20, show=5, lr=0.5)
+    test_predictions = model.predict(test_dataset[0])
+    test_predictions_dataframe = pd.DataFrame(test_predictions, columns=['x', 'y'])
+
+    solution_predictions = model.predict(solution_measurements)
+    solution_predictions_dataframe = pd.DataFrame(solution_predictions, columns=['x', 'y'])
+
+    sns.relplot(x='x', y='y', data=solution_predictions_dataframe)
+    sns.relplot(x='reference x', y='reference y', data=solution_references)
+    sns.relplot(x='measurement x', y='measurement y', data=solution_measurements)
+    plt.show()
