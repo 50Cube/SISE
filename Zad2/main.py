2bfbcc0d97d3a4a0f8ea72ed32548a86eb7abb94
Jakub Bogdan <kubab1998@o2.pl>
2020-05-03 16:38:55 +0200

    Nauka na wszystkich plikach

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,8 +1,6 @@
 import FileOperations
 
 import pandas as pd
-import numpy as np
-import pathlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
@@ -11,12 +9,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 import tensorflow_docs as tfdocs
-import tensorflow_docs.plots
 import tensorflow_docs.modeling
-
-
-def calculate_outputs(dataframe, threshold):
-    dataframe['output'] = [1 if x < threshold else 0 for x in dataframe['distance']]
 
 
 def build_model():
@@ -24,7 +17,9 @@ def build_model():
     # funkcja aktywacji relu - rectified linear unit, f(z) is zero when z is less than zero and f(z) is equal to z when z is above or equal to zero.
     # input shape = liczba kolumn w dataframe
     model = keras.Sequential([
-        layers.Dense(64, activation='relu', input_shape=[len(train_dataset[0].keys())]),
+        layers.Dense(8, activation='relu', input_shape=[len(train_dataset_measurement[0].keys())]),
+        layers.Dense(16, activation='relu'),
+        layers.Dense(32, activation='relu'),
         layers.Dense(64, activation='relu'),
         layers.Dense(2)
     ])
@@ -38,50 +33,35 @@ def build_model():
 
 
 if __name__ == '__main__':
-    runs = []
-    train_values_measurements = []
-    train_values_references = []
-    outputs = []
     solution = pd.DataFrame
     solution_measurements = pd.DataFrame
     solution_references = pd.DataFrame
-    train_dataset = []
-    test_dataset = []
-    train_dataset_labels = []
-    test_dataset_labels = []
+    train_dataset_measurement = []
+    train_dataset_reference = []
+    train_datasets_measurement = pd.DataFrame
+    train_datasets_reference = pd.DataFrame
 
     for i in range(12):
-        runs.append(FileOperations.FileOperations.load_data_from_excel
+        train_dataset_measurement.append(FileOperations.FileOperations.load_data_from_excel
                     (r'pozyxAPI_dane_pomiarowe\pozyxAPI_only_localization_measurement' + str(i+1) + '.xlsx', "measurement", "D:H"))
-        # train_values_measurements.append(runs[i][['measurement x', 'measurement y']])
-        # train_values_references.append(runs[i][['reference x', 'reference y']])
-        train_dataset.append(runs[i].sample(frac=0.8, random_state=0))
-        test_dataset.append(runs[i].drop(train_dataset[i].index))
-        train_dataset_labels.append(train_dataset[i][['reference x', 'reference y']])
-        test_dataset_labels.append(test_dataset[i][['reference x', 'reference y']])
-        train_dataset[i] = train_dataset[i][['measurement x', 'measurement y']]
-        test_dataset[i] = test_dataset[i][['measurement x', 'measurement y']]
+        train_dataset_reference.append(train_dataset_measurement[i][['reference x', 'reference y']])
+        train_dataset_measurement[i] = train_dataset_measurement[i][['measurement x', 'measurement y']]
 
     solution = FileOperations.FileOperations.load_data_from_excel(r'pozyxAPI_only_localization_dane_testowe_i_dystrybuanta.xlsx', "pomiar", "D:M")
     solution_measurements = solution[['measurement x', 'measurement y']].dropna()
     solution_references = solution[['reference x', 'reference y']].dropna()
 
+    train_datasets_measurement = pd.concat(train_dataset_measurement, ignore_index=True)
+    train_datasets_reference = pd.concat(train_dataset_reference, ignore_index=True)
+
     model = build_model()
-    model.summary()
-    # The patience parameter is the amount of epochs to check for improvement
-    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    # model.summary()
 
     # validation split - Fraction of the training data to be used as validation data
     # verbose: Integer. 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
     # tfdocs.EpochDots simply prints a . for each epoch, and a full set of metrics every 100 epochs.
     # EarlyStopping callback tests a training condition for every epoch. If a set amount of epochs elapses without showing improvement, then it stops the training
-    history = model.fit(train_dataset[0], train_dataset_labels[0], epochs=20, validation_split=0.2, verbose=0, callbacks=[early_stop, tfdocs.modeling.EpochDots()])
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
-    hist.tail()
-
-    test_predictions = model.predict(test_dataset[0])
-    test_predictions_dataframe = pd.DataFrame(test_predictions, columns=['x', 'y'])
+    history = model.fit(train_datasets_measurement, train_datasets_reference, epochs=300, validation_split=0.1, verbose=0, callbacks=[tfdocs.modeling.EpochDots()])
 
     solution_predictions = model.predict(solution_measurements)
     solution_predictions_dataframe = pd.DataFrame(solution_predictions, columns=['x', 'y'])
